67f08a9dc9a62d8dd02c2442a828ab95174f1a5f
Reda <wayzfut@gmail.com>
2026-06-14 22:05:51 -0700

    Initial Commit

---
+++ techbrosbudget/techbrosbudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  techbrosbudgetApp.swift
+//  techbrosbudget
+//
+//  Created by Reda Boutayeb on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct techbrosbudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ techbrosbudgetTests/techbrosbudgetTests.swift
@@ -0,0 +1,19 @@
+//
+//  techbrosbudgetTests.swift
+//  techbrosbudgetTests
+//
+//  Created by Reda Boutayeb on 6/14/26.
+//
+
+import Testing
+@testable import techbrosbudget
+
+struct techbrosbudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
